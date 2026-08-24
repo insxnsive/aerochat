32b207ad9d062623be83276d6b164c5de7434c74
Nate <83441712+insxnsive@users.noreply.github.com>
2026-08-24 19:15:26 -0300

    refactor: bind Home presentation state

--- Aerochat.VisualShell.Tests/HomeShellTests.cs
+++ Aerochat.VisualShell.Tests/HomeShellTests.cs
@@ -1,5 +1,6 @@
 using Aerochat.Presentation;
 using Aerochat.Windows;
+using System.IO;
 
 namespace Aerochat.VisualShell.Tests;
 
@@ -69,5 +70,45 @@ public sealed class HomeShellTests
 
         Assert.That(localization.LanguageCode, Is.EqualTo("en-US"));
         Assert.That(localization["MissingKey"], Is.EqualTo("MissingKey"));
+    }
+
+    [Test]
+    public void Home_xaml_uses_presentation_state_bindings_without_visual_drift()
+    {
+        string homePath = Path.GetFullPath(Path.Combine(
+            TestContext.CurrentContext.TestDirectory,
+            "../../../../../Aerochat/Windows/Home.xaml"));
+        string xaml = File.ReadAllText(homePath);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(xaml, Does.Not.Contain("Theme.Scene"));
+            Assert.That(xaml, Does.Not.Contain("FilteredCategories"));
+            Assert.That(xaml, Does.Not.Contain("xmlns:viewmodels"));
+            Assert.That(xaml, Does.Not.Contain("HomeWindowViewModel"));
+            Assert.That(xaml, Does.Contain("d:DataContext=\"{d:DesignInstance Type=presentation:PresentationState}\""));
+            Assert.That(xaml, Does.Contain("CurrentScene.Color"));
+            Assert.That(xaml, Does.Contain("CurrentScene.TextColor"));
+            Assert.That(xaml, Does.Contain("CurrentScene.File"));
+            Assert.That(xaml, Does.Contain("CurrentScene.IsDefault"));
+            Assert.That(xaml, Does.Contain("CurrentUser.Presence.Status"));
+            Assert.That(xaml, Does.Contain("Person.Presence.Status"));
+            Assert.That(xaml, Does.Contain("Person.Presence.Activity"));
+            Assert.That(xaml, Does.Contain("FilteredContactGroups"));
+            Assert.That(xaml, Does.Contain("Settings.ShowAds"));
+            Assert.That(xaml, Does.Contain("Settings.ShowNews"));
+            Assert.That(xaml, Does.Contain("Settings.ShowEyecandy"));
+            Assert.That(xaml, Does.Contain("News[0].Body"));
+            Assert.That(xaml, Does.Contain("Notices[0].Message"));
+            Assert.That(xaml, Does.Contain("CurrentAd.ImageUri"));
+
+            Assert.That(xaml, Does.Contain("Height=\"650\" Width=\"400\""));
+            Assert.That(xaml, Does.Contain("MinWidth=\"300\""));
+            Assert.That(xaml, Does.Contain("MinHeight=\"500\""));
+            Assert.That(xaml, Does.Contain("/Aerochat;component/Resources/Home/SearchBar.png"));
+            Assert.That(xaml, Does.Contain("/Aerochat;component/Resources/Home/TreeHover.png"));
+            Assert.That(xaml, Does.Contain("/Aerochat;component/Resources/Message/Separator.png"));
+            Assert.That(xaml, Does.Contain("/Aerochat;component/Resources/Message/Background.png"));
+        });
     }
 }
